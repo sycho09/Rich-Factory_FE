59a795da533aeb8cd6d44f95ab92b2ef3b684f05
sycho09 <wyswid@naver.com>
2023-02-01 12:07:36 +0900

    fix: use render method in index file to apply absolute paths

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,15 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { CookiesProvider } from "react-cookie";
 import { BrowserRouter } from "react-router-dom";
 
-const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
-);
-root.render(
+ReactDOM.render(
   <CookiesProvider>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </CookiesProvider>
+  </CookiesProvider>,
+  document.getElementById("root")
 );
