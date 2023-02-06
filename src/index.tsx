eb6f13b93c58c3ab869c30f18394710e50574ba5
sycho09 <wyswid@naver.com>
2023-02-07 01:10:50 +0900

    fix: react dom version error fixed

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,25 @@
 import React from "react";
-import ReactDOM from "react-dom";
+// import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { CookiesProvider } from "react-cookie";
 import { BrowserRouter } from "react-router-dom";
 
-ReactDOM.render(
+const container = document.getElementById("root") as HTMLElement;
+const root = createRoot(container);
+root.render(
   <CookiesProvider>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </CookiesProvider>,
-  document.getElementById("root")
+  </CookiesProvider>
 );
+// ReactDOM.render(
+//   <CookiesProvider>
+//     <BrowserRouter>
+//       <App />
+//     </BrowserRouter>
+//   </CookiesProvider>,
+//   document.getElementById("root")
+// );
