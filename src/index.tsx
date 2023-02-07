681f50f39fa98d339bad386a518d3bd31455f59c
sycho09 <wyswid@naver.com>
2023-02-07 10:03:28 +0900

    fix: react version downgrade from 18 to 17 to solve exit code 1

--- src/index.tsx
+++ src/index.tsx
@@ -1,25 +1,25 @@
 import React from "react";
-// import ReactDOM from "react-dom";
-import { createRoot } from "react-dom/client";
+import ReactDOM from "react-dom";
+// import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { CookiesProvider } from "react-cookie";
 import { BrowserRouter } from "react-router-dom";
 
-const container = document.getElementById("root") as HTMLElement;
-const root = createRoot(container);
-root.render(
-  <CookiesProvider>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </CookiesProvider>
-);
-// ReactDOM.render(
+// const container = document.getElementById("root") as HTMLElement;
+// const root = createRoot(container);
+// root.render(
 //   <CookiesProvider>
 //     <BrowserRouter>
 //       <App />
 //     </BrowserRouter>
-//   </CookiesProvider>,
-//   document.getElementById("root")
+//   </CookiesProvider>
 // );
+ReactDOM.render(
+  <CookiesProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </CookiesProvider>,
+  document.getElementById("root")
+);
